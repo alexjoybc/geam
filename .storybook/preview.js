c8ff426fb1f70915412c703ac29bb0e7a05ede4b
alexjoybc <51387119+alexjoybc@users.noreply.github.com>
2020-05-21 04:32:33 +0000

    Update gh-pages to output generated at a5ddeef

--- .storybook/preview.js
+++
@@ -1,2 +0,0 @@
-import "@coreui/coreui/dist/css/bootstrap.min.css"
-import "@coreui/coreui/dist/css/coreui.min.css"
--- src/components/issue/card/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function IssueCard({ issue: { id, title, state } }) {
-  return (
-    <div>
-      {id} - {title} - {state}
-    </div>
-  );
-}
